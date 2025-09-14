c37d37899bd81be880346928925cb69c88d84e47
Pulkit-bristol <gl24171@bristola.c.uk>
2025-09-14 12:28:44 +0100

    adding terraform network

--- infra/variables.tf
+++ infra/variables.tf
@@ -14,3 +14,33 @@ variable "project_name" {
   type    = string
   default = "py-ecs"
 }
+variable "image_url" {
+  description = "Container image URI (ECR/GHCR)."
+  type        = string
+  default     = "217262486301.dkr.ecr.us-east-1.amazonaws.com/test_flask/pulkit"
+}
+variable "app_name" {
+  description = "Application name (used in resource names)."
+  type        = string
+  default     = "flask-app"
+}
+
+variable "container_port" {
+  type        = number
+  default     = 5000
+}
+
+variable "desired_count" {
+  type        = number
+  default     = 1
+}
+
+variable "cpu" {
+  type        = number
+  default     = 256
+}
+
+variable "memory" {
+  type        = number
+  default     = 512
+}

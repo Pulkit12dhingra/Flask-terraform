40b17e40e36bcd3ed8b078ac31ad24c304ace950
Pulkit-bristol <gl24171@bristola.c.uk>
2025-09-14 12:25:34 +0100

    adding terraform network

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,16 @@
-variable region {
+variable "region" {
   type        = string
   default     = "us-east-1"
-  description = "defines the region"
+  description = "region Variable"
+}
+
+variable "availablity_zone" {
+  type        = string
+  default     = "us-east-1a"
+  description = "region Variable"
+}
+
+variable "project_name" {
+  type    = string
+  default = "py-ecs"
 }
